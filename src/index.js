6e7f7860b81ec4afe54dcd94285b0639260af90f
Dmitriy Savranin <dsavranin@gmail.com>
2020-02-22 17:51:29 +0500

    refactor: add my solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  let res = 0;
+  if (array === undefined || array.length == 0) {
+    return res;
+  }
+  for (let element of array) {
+    res = Math.min(res, element);
+  }
+  return res;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  let res = 0;
+  if (array === undefined || array.length == 0) {
+    return res;
+  }
+  for (let element of array) {
+    res = Math.max(res, element);
+  }
+  return res;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  let res = 0;
+  let sum = 0;
+  if (array === undefined || array.length == 0) {
+    return res;
+  }
+  for (let element of array) {
+    sum = sum + element;
+  }
+  res = sum / array.length;
+  return res;
 }
+
+
